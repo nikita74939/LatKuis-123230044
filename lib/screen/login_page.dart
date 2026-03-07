bcc64331f7e7f358de53c697da97062569bf4669
nikita74939 <itanikita20005@gmail.com>
2026-03-08 04:23:18 +0700

    login done

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,19 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:latkuis/components/text_field.dart'; 
+import 'package:latkuis/models/user.dart';
+import 'package:latkuis/screen/movie_list_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController userC = TextEditingController();
+  final TextEditingController passC = TextEditingController();
+  bool isLoggedin = false;
+  String errorMessage = ""; 
+
+  void _login() {
+    setState(() {
+      if (userC.text == user1.username && passC.text == user1.password) {
+        errorMessage = "";
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MovieListPage(name: user1.name),
+          ),
+        );
+      } else {
+        errorMessage = "salah oi! sini kubisikin, un: nikita & pass: 044";
+
+        // Bisa juga pake logika:
+        if (userC.text.isEmpty || passC.text.isEmpty) {
+          errorMessage = "jangan dikosongin dong";
+        }
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    /*
-      buatlah tampilan login sederhana dengan menggunakan widget TextField untuk input username dan password, serta ElevatedButton untuk tombol login.
-      - TextField untuk username dengan hint text "Username"
-      - TextField untuk password dengan hint text "Password" dan obscureText true
-      - ElevatedButton dengan text "Login" yang ketika ditekan akan menampilkan snackbar dengan pesan
-        "Login berhasil" jika username dan password tidak kosong, atau "Login gagal" jika salah satu atau kedua field kosong.
-      - jika login berhasil, navigasikan ke halaman MovieListPage (buat halaman MovieListPage 
-    
-     */
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+
+            // image
+            Image.asset('assets/login_img.png', width: 300),
+
+            const SizedBox(height: 30),
+
+            Text(
+              'Login dulu ya!',
+              style: GoogleFonts.poppins(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            // error message
+            if (errorMessage.isNotEmpty)
+              Text(
+                errorMessage,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.redAccent,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+
+            const SizedBox(height: 20),
+
+            // username field
+            MyTextField(
+              controller: userC,
+              hintText: 'Username',
+              obsecureText: false,
+            ),
+
+            const SizedBox(height: 20),
+
+            // password field
+            MyTextField(
+              controller: passC,
+              hintText: 'Password',
+              obsecureText: true,
+            ),
+
+            const SizedBox(height: 20),
+
+            // button login
+            ElevatedButton(
+              onPressed: _login,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.white,
+                minimumSize: const Size(double.infinity, 54),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Text(
+                'Login',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
   }
 }
